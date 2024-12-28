d21e1c7e5678719d3fbaf31c5a81970c4c92b287
fairofif <fairofif@gmail.com>
2024-12-28 10:35:37 +0700

    handle email exists in register

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,12 +6,16 @@ const api = axios.create({
 
 export const restRegister = async (userData) => {
     try {
-        const response = await api.post('/auth/register', userData)
-        return response.data.data
+        const response = await api.post('/auth/register', userData);
+        return response.data.data;
     } catch (e) {
-        throw new Error('Failed to register: ' + e.message)
+        if (e.response && e.response.status === 500) {
+            throw new Error('Email already registered');
+        } else {
+            throw new Error('Failed to register: ' + e.message);
+        }
     }
-}
+};
 
 export const restLogin = async (userData) => {
     try {
